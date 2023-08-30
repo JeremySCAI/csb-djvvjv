adc3328975ef81b320900e1036378b77b879e3da
Code Sandbox <hello@codesandbox.io>
2023-08-30 08:37:49 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.2914a30b2de6716fe837449bd22198b5.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fdb03522f286809a13452fc1d5ecd1d",
+    "url": "https://.github.io/csb-djvvjv/index.html"
+  },
+  {
+    "revision": "102a4ad86bb19069e978",
+    "url": "https://.github.io/csb-djvvjv/static/css/main.73c7ff7b.chunk.css"
+  },
+  {
+    "revision": "f50547cf53d874d2b20d",
+    "url": "https://.github.io/csb-djvvjv/static/js/2.9314143e.chunk.js"
+  },
+  {
+    "revision": "102a4ad86bb19069e978",
+    "url": "https://.github.io/csb-djvvjv/static/js/main.970b7f2d.chunk.js"
+  },
+  {
+    "revision": "3cf7328b6fecd8eb575d",
+    "url": "https://.github.io/csb-djvvjv/static/js/runtime~main.a259b280.js"
+  },
+  {
+    "revision": "db57f379ee8c18be9851cb4aeda6de6c",
+    "url": "https://.github.io/csb-djvvjv/static/media/audio.db57f379.gif"
+  },
+  {
+    "revision": "14903a2aa9cded81c62531c97d717cb2",
+    "url": "https://.github.io/csb-djvvjv/static/media/bud.14903a2a.png"
+  },
+  {
+    "revision": "09d5abfbd8c6c7fc15d3abd50feb67cf",
+    "url": "https://.github.io/csb-djvvjv/static/media/emilyBackground.09d5abfb.jpg"
+  },
+  {
+    "revision": "fade391b65aecd875cd688f8a74780e1",
+    "url": "https://.github.io/csb-djvvjv/static/media/icon.fade391b.png"
+  },
+  {
+    "revision": "bb5d4b089257a4a4dc50e5acba4af05d",
+    "url": "https://.github.io/csb-djvvjv/static/media/logo.bb5d4b08.png"
+  },
+  {
+    "revision": "2f05aee1e208806620563f7d9a5dd647",
+    "url": "https://.github.io/csb-djvvjv/static/media/manonbeach.2f05aee1.png"
+  },
+  {
+    "revision": "7e2ca13de440c9b33e89912c9296b1c3",
+    "url": "https://.github.io/csb-djvvjv/static/media/popupemily.7e2ca13d.png"
+  },
+  {
+    "revision": "dd736258c8f92f63ae09597878f2c91a",
+    "url": "https://.github.io/csb-djvvjv/static/media/voice.dd736258.gif"
+  }
+]);
